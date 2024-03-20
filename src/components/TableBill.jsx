7717604f7d3c656daccd851f0c8d130d939d8d60
NagarajanSanthosh <64208ns@gmail.com>
2024-03-20 17:12:19 +0530

    tablebill component created

---
+++ src/components/TableBill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TableBill = () => {
+    return (
+        <div>
+            <h3>Adyar Anandha Bhavan</h3>
+            
+        </div>
+    )
+}
+
+export default TableBill
