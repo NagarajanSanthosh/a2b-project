6c490d1436c907eb96d2658864ce793e78585171
123Pandimeena <meena2208nirai@gmail.com>
2024-03-22 12:07:16 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import Addtocart from  './components/Addtocart';
-import OrderPlaced from  './components/OrderPlaced';
-import Navbar from './components/Navbar';
-import Tabletime from './components/TableTime';
-import Homepage from './components/Homepage';
+import Addtocart from './components/Addtocart';
+import OrderPlaced from './components/OrderPlaced';
+import Menu from './components/Menu';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TableTime from './components/TableTime';
 
 function App() {
   return (
+
     <div className="App">
-      <Homepage/>
+      <h1>Hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Menu />}>
+          </Route>
+          <Route path="/addtocart" element={<Addtocart />}>
+          </Route>
+          <Route path="/tablebill" element={<TableTime />}>
+          </Route>
+          <Route path="/success" element={<OrderPlaced />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
+
   );
 }
 
